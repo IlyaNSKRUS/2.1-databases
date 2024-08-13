90109cf73da2bc0e107ff1464e1ca092629fabe1
Ilya NSK <Ilyagl.nsk@yandex.ru>
2024-08-13 20:23:47 +0700

    Phone_expert_code

--- work_with_database/phones/views.py
+++ work_with_database/phones/views.py
@@ -6,21 +6,44 @@ def index(request):
     return redirect('catalog')
 
 
+# def show_catalog(request):
+#     template = 'catalog.html'
+#     sort = request.GET.get('sort')
+#     print(sort)
+#     if sort == 'name':
+#         phones_all = Phone.objects.order_by('name')
+#     elif sort == 'min_price':
+#         phones_all = Phone.objects.order_by('price')
+#     elif sort == 'max_price':
+#         phones_all = Phone.objects.order_by('-price')
+#     else:
+#         phones_all = Phone.objects.all()
+#     context = {
+#         'phones': phones_all,
+#     }
+#     return render(request, template, context)
+
 def show_catalog(request):
+    sort = request.GET.get('sort','name')
     template = 'catalog.html'
-    sort = request.GET.get('sort')
-    if sort == 'name':
-        phones_all = Phone.objects.order_by('name')
-    elif sort == 'min_price':
-        phones_all = Phone.objects.order_by('price')
-    elif sort == 'max_price':
-        phones_all = Phone.objects.order_by('-price')
-    else:
-        phones_all = Phone.objects.all()
+    print(sort)
+
+    def get_sort_param(sort):
+        if sort == 'name':
+            return 'name'
+        if sort == 'min_price':
+            return 'price'
+        elif sort == 'max_price':
+            return '-price'
+        else:
+            raise ValueError(f'Некорректный параметр сортировки: {sort}')
+
     context = {
-        'phones': phones_all,
+        'phones': Phone.objects.order_by(get_sort_param(sort)),
     }
+
     return render(request, template, context)
+
 
 
 def show_product(request, slug):
